cf4f42af9afa895107eaac4d0e04d0e2582cc772
Tejas Ghag <tejas.ghag@somaiya.edu>
2026-05-24 07:57:29 +0000

    Data Cleaning and Preprocessing

--- Project1.py
+++ Project1.py
@@ -2,9 +2,14 @@ import pandas as pd
 
 #TASK 1 
 df = pd.read_csv('loan.csv')
-df.head()
-df.info()
-df.describe()
+df.head()      #Gives initial 5 rows of the dataset
+df.info()      #Gives the summary of the dataset, including data types and non-null counts
+df.describe()  #Provides statistical summary of numerical columns in the dataset
 
+#TASK 2
+print("\nMissing Values:")
+print(df.isnull().sum())
 
+print("\nDuplicate Rows:")
+print(df.duplicated().sum())
 
